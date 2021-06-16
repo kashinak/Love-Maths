2f9dccba3e8cb8e83f7d6caf216d24eae7b85a42
kashinak <kashinakfx@gmail.com>
2021-06-16 20:16:53 +0000

    pass argument gameType into runGame function checking gameType parameter to see if it is equal to addition and if so will display addition question

--- script.js
+++ script.js
@@ -10,19 +10,28 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
-function runGame() {
+function runGame(gameType) {
     // Generate two random numbers between 1 and 25
     // Math.floor rounds down to the whole number
     // Math.random generates random numbers
 
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting!`;
+    }
 }
 
 function checkAnswer() {
