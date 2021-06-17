aa4fd77890a3a22a6b798d1ee8685524491f4e0d
kashinak <kashinakfx@gmail.com>
2021-06-17 04:15:20 +0000

    create displayDivideQuestion function to ensure operand 1 is greater than operand 2

--- script.js
+++ script.js
@@ -137,3 +137,9 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "x";
 
 }
+
+function displayDivideQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "/";
+}
